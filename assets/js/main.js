67ed066cb3fc1e9c0e8b33e7efd7dbd9878b3c9b
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-09-30 10:11:53 -0300

    Init the JS file

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const readAll = document.querySelector('[data-button="readAll"]')
+const notificationList = document.querySelector('ul.notifications-list')
+
+readAll.addEventListener('click', (e) => {
+    e.preventDefault()
+    
+})
