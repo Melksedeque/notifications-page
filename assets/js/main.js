c6537d64bf026af5b2f62d4f03ad48ef4b695b31
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-10-01 01:01:24 -0300

    Function to get the quantity of new notifications

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,20 @@
-const readAll = document.querySelector('[data-button="readAll"]')
+const btnReadAll = document.querySelector('[data-button="readAll"]')
 const notifications = document.querySelectorAll('li.notification')
+const unreadNotifications = document.querySelectorAll('li.notification.unread')
+const notificationNumber = document.querySelector('.unread-qnt')
 
-readAll.addEventListener('click', () => {
+newNotificationsQnt()
+
+btnReadAll.addEventListener('click', () => {
     notifications.forEach(notification => {
         const mark = document.querySelector('.new-post-mark')
         mark.parentElement.removeChild(mark)
         notification.classList.remove('unread')
     })
+    newNotificationsQnt()
 })
+
+
+function newNotificationsQnt() {
+    notificationNumber.innerText = unreadNotifications.length
+}
