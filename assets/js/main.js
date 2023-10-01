41347b3011bb475e3a88686723d7b438e41b2cf3
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-10-01 00:54:16 -0300

    Function to remove the unread class and the new-post marker

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,10 @@
 const readAll = document.querySelector('[data-button="readAll"]')
-const notificationList = document.querySelector('ul.notifications-list')
+const notifications = document.querySelectorAll('li.notification')
 
-readAll.addEventListener('click', (e) => {
-    e.preventDefault()
-    
+readAll.addEventListener('click', () => {
+    notifications.forEach(notification => {
+        const mark = document.querySelector('.new-post-mark')
+        mark.parentElement.removeChild(mark)
+        notification.classList.remove('unread')
+    })
 })
